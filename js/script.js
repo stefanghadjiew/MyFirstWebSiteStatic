e9ff4500b941f54a2e8e60a930ebff3bcd3768db
stefanghadjiew <stefanghadjiew@abv.bg>
2020-09-28 23:32:19 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const body = document.querySelector('[data-body]');
 const logInDiv = document.querySelector('[data-log-in-div]');
 const signUpDiv = document.querySelector('[data-sign-up]');
 const homeBtn = document.querySelector('[data-home-btn]');
-/* const url = "https://mongodb+srv://cheffo0o:<password>@cluster0.qus4z.mongodb.net/MyFirstWebSiteDb?retryWrites=true&w=majority/api" */
+const url = "https://aqueous-castle-19920.herokuapp.com/"
 
 homeBtn.addEventListener("click",(e) => {
     e.preventDefault()
@@ -27,7 +27,7 @@ async function checkBagProducts() {
     try{
         const userId = localStorage.getItem("userId")
         const token = localStorage.getItem("token")
-        const bagUrl = `api/user/${userId}/products`
+        const bagUrl = `${url}api/user/${userId}/products`
         const res = await fetch(bagUrl, {
                 method: "GET",
                 headers : {
@@ -101,7 +101,7 @@ addToBagBtns.forEach(btn => {
             addProduct()
             async function addProduct() {
                 try {
-                    const addProductUrl = `api/user/${userId}/products` 
+                    const addProductUrl = `${url}api/user/${userId}/products` 
                     const product = {
                         quantity: 1,
                         src: imgSrc,
@@ -135,7 +135,7 @@ async function getProduct() {
     try{
         const userId = localStorage.getItem("userId");
         const token = localStorage.getItem("token");
-        const getProductsUrl = `api/user/${userId}/products` 
+        const getProductsUrl = `${url}api/user/${userId}/products` 
         await fetch(getProductsUrl, {
             method: "GET",
             headers: {
@@ -152,7 +152,7 @@ async function deleteProduct() {
     try {
         const userId = localStorage.getItem("userId");
         const token = localStorage.getItem("token");
-        const deleteProductsUrl = `api/user/${userId}/products` 
+        const deleteProductsUrl = `${url}api/user/${userId}/products` 
         await fetch(deleteProductsUrl, {
             method: "DELETE",
             headers: {
@@ -323,7 +323,7 @@ function UserReg(firstName, lastName, email, password) {
 
 formReg.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const register = `api/auth/register`
+    const register = `${url}api/auth/register`
     const userInput = new UserReg(firstName, lastName, email, password)
     if (checkPasswordMatch(password, repeatPassword) === true) {
         try {
@@ -365,7 +365,7 @@ function UserLog(email, password) {
 
 formLog.addEventListener("submit", async (e) => {
     e.preventDefault()
-    const login = `api/auth/login`
+    const login = `${url}api/auth/login`
     const userLog = new UserLog(emailLog, passwordLog)
     
         const response = await
@@ -431,7 +431,7 @@ async function displayCartContent() {
         return
     }
     try{
-        const getProducts = `api/user/${userId}/products`
+        const getProducts = `${url}api/user/${userId}/products`
         const res = await fetch(getProducts, {
             method: "GET",
             headers : {
